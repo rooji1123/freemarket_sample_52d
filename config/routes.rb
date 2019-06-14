df13891e695afc3eb06e892d3983f18fc609ba95
加藤蒔朗 <rooji@katounoMacBook-Air.local>
2019-06-14 13:48:32 +0900

    update_

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_for :users
   root 'items#index'
+  resources :users, only:[:show, :edit]
   resources :items
+  resources :categories, only:[:index, :show]
+  resources :brands, only:[:index, :show]
+  scope module: 'users' do
+    resources :user_cards, only:[:edit, :update]
+    resources :user_identifications, only:[:edit, :update]
+  end
 end
