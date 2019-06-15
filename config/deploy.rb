7cb6f0061a8ccf3b45b37070150ca0a435e0f20d
加藤蒔朗 <rooji@katounoMacBook-Air.local>
2019-06-15 14:45:13 +0900

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock "~> 3.11.0"
 
 set :application, "freemarket_sample_52d_2"
-set :repo_url, "git@example.com:rooji1123/freemarket_sample_52d_2.git"
+set :repo_url, "git@github.com:rooji1123/freemarket_sample_52d_2.git"
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 set :rbenv_type, :user
 set :rbenv_ruby, '2.5.1'
