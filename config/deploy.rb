efb5211259550aa866044f84ccc2978c9c7fea19
加藤蒔朗 <rooji@katounoMacBook-Air.local>
2019-06-15 13:55:20 +0900

    crate_capistrano

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,7 +1,7 @@
 # config valid for current version and patch releases of Capistrano
 lock "~> 3.11.0"
 
-set :application, "freemarket_sample_52d"
+set :application, "freemarket_sample_52d_2"
 set :repo_url, "git@example.com:rooji1123/freemarket_sample_52d_2.git"
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 set :rbenv_type, :user
