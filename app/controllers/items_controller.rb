25d517458f21c248dbd34f21cbffa8fe50f3204c
masatechexpert <cate35cate@gmail.com>
2019-06-17 17:51:36 +0900

    items/new
    商品出品機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -7,12 +7,22 @@ class ItemsController < ApplicationController
 
   def new
     @item = Item.new
+    10.times { @item.item_images.build }
   end
 
   def create
-    @item = Item.new(item_params)
-    @item.save
-    redirect_to new_item_path
+     @item = Item.new(item_params)
+    respond_to do |format|
+      if @item.save
+          params[:item_images][:image].each do |image|
+            @item.item_images.create(image: image, item_id: @item.id)
+          end
+        format.html{redirect_to root_path}
+      else
+        @item.item_images.build
+        format.html{render action: 'new'}
+      end
+    end
   end
 
   def edit
@@ -27,6 +37,6 @@ class ItemsController < ApplicationController
  private
 
    def item_params
-     params.require(:item).permit(:name)
+     params.require(:item).permit(:name, :description, :prefecture_id, :price, :delivery_date_id, :delivery_fee_id, :item_state_id, item_images_attributes: [:image])
    end
  end
