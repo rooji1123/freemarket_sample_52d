b54005278de0cb8bb0faaad6e93418f3ba5066d6
masayoshiishitsuka <52.ishitsuka.masayoshi>
2019-06-17 19:00:55 +0900

    add controller

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,6 +3,10 @@ class ItemsController < ApplicationController
   end
 
   def show
+    @item = Item.find(params[:id])
+    @selleritems = Item.where(seller_id_id: @item.seller_id_id).where.not(id: @item.id)
+    @categoryitems = Item.where(category_id: @item.category_id).where.not(id: @item.id)
+    @images = @item.images.where(params[:item_id])
   end
 
   def new
