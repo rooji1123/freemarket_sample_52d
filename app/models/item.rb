78e13fafdaabfd9da912b093433507e74d6baa54
masayoshiishitsuka <52.ishitsuka.masayoshi>
2019-06-19 10:59:04 +0900

    add models

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,15 +1,5 @@
 class Item < ApplicationRecord
-  # validates :name, :description, :price, presence: true
-  # validates :name,         length: { maximum: 40 }
-  # validates :price,        length: { in: 300..9999999 } 
-  # validates :description, length: { maximum: 1000 }
+  has_many :comments, dependent: :destroy
   has_many :item_images, dependent: :destroy
-  accepts_nested_attributes_for :item_images
-
-  extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to_active_hash :prefecture
-  belongs_to_active_hash :delivery_fee
-  belongs_to_active_hash :delivery_date
-  belongs_to_active_hash :item_state
-
+  has_one :delivery, dependent: :destroy
 end
