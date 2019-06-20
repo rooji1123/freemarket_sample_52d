3abc680ad8fcc308e42c52ec022db1e22780296b
加藤蒔朗 <rooji@katounoMacBook-Air.local>
2019-06-20 20:28:30 +0900

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_19_065023) do
+ActiveRecord::Schema.define(version: 2019_06_20_034137) do
 
   create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -37,8 +37,14 @@ ActiveRecord::Schema.define(version: 2019_06_19_065023) do
     t.index ["item_id"], name: "index_deliveries_on_item_id"
   end
 
+  create_table "images", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "item_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "item_images", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "image"
+    t.string "item_image"
     t.bigint "item_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -49,22 +55,19 @@ ActiveRecord::Schema.define(version: 2019_06_19_065023) do
     t.string "name"
     t.text "description"
     t.integer "price"
-    t.integer "size", default: 0, null: false
+    t.integer "size"
     t.integer "item_state"
     t.bigint "seller_id"
     t.bigint "buyer_id"
     t.integer "likes"
     t.bigint "brand_id"
+    t.bigint "category_id"
     t.integer "deal_state"
-    t.string "item_state_id", null: false
-    t.string "delivery_fee_id", null: false
-    t.string "prefecture_id", null: false
-    t.string "delivery_date_id", null: false
-    t.string "delivery_choice_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["brand_id"], name: "index_items_on_brand_id"
     t.index ["buyer_id"], name: "index_items_on_buyer_id"
+    t.index ["category_id"], name: "index_items_on_category_id"
     t.index ["name"], name: "index_items_on_name"
     t.index ["seller_id"], name: "index_items_on_seller_id"
   end
@@ -74,6 +77,27 @@ ActiveRecord::Schema.define(version: 2019_06_19_065023) do
     t.integer "category_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "notices", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.integer "user_id"
+    t.text "text"
+    t.integer "item_id"
+    t.integer "read"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "deliveries", "items"
