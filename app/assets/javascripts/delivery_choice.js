cd8f289ba26a5be593b3c2cfecc049c27e807f87
masatechexpert <cate35cate@gmail.com>
2019-06-20 16:34:45 +0900

    Update delivery_choice.js

--- app/assets/javascripts/delivery_choice.js
+++ app/assets/javascripts/delivery_choice.js
@@ -1,3 +1,10 @@
+$(function () {
+  $("#item_delivery_fee_id").one("change", function () {
+    $('.sell-form__delivery__form-box__choice').css({
+      'display': 'block'
+    });
+    $('#item_delivery_choice_id').css({
+      'display': 'block'
     });
   });
 })
