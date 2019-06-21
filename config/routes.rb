77f98764a41620b8af2aae26173f2ee3272ec7c8
加藤蒔朗 <rooji@katounoMacBook-Air.local>
2019-06-21 17:22:34 +0900

    edit

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,13 @@ Rails.application.routes.draw do
   resources :items
   resources :categories, only:[:index, :show]
   resources :brands, only:[:index, :show]
+  resources :users, onry: [:show] do
+    scope module: 'users' do
+      resources :user_cards, only:[:edit, :update]
+      resources :user_identifications, only:[:edit, :update]
+      resources :exhibitions, only:[:index, :show, :edit, :update]
+    end
+  end
   scope module: 'users' do
     resources :user_cards, only:[:edit, :update]
     resources :user_identifications, only:[:edit, :update]
