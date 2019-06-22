295ce0518dea550452d8faa13cb85ff957ba2bff
加藤蒔朗 <rooji@katounoMacBook-Air.local>
2019-06-22 18:25:53 +0900

    edit-product

--- app/controllers/users/exhibitions_controller.rb
+++ app/controllers/users/exhibitions_controller.rb
@@ -1,2 +1,22 @@
 class Users::ExhibitionsController < ApplicationController
+  def index
+    @items = Item.where(seller_id: 1)
+  end
+
+  def show
+    @item = Item.find(set_item)
+    @selleritems = Item.where(seller_id: @item.seller_id).where.not(id: @item.id)
+    # @categoryitems = Item.where(category_id: categories.category_id).where.not(id: @item.id)
+    @images = @item.item_images.where(params[:id])
+  end
+
+  def edit
+    
+  end
+
+  private
+
+  def set_item
+    params.require(:id)
+  end
 end
