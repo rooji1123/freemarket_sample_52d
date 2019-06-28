5abc90fb3e93ee308c663d0e7809d28e0d3802ce
加藤蒔朗 <rooji@katounoMacBook-Air.local>
2019-06-28 20:59:05 +0900

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -26,8 +26,8 @@ append :linked_files, 'config/master.key'
 set :default_env, {
   rbenv_root: "/usr/local/rbenv",
   path: "/usr/local/rbenv/shims:/usr/local/rbenv/bin:$PATH",
-  AWS_ACCESS_KEY_ID: ENV["AWS_ACCESS_KEY_ID"],
-  AWS_SECRET_ACCESS_KEY: ENV["AWS_SECRET_ACCESS_KEY"]
+  AWS_ACCESS_KEY_ID: ENV["AWS__KEY_ID"],
+  AWS_SECRET_ACCESS_KEY: ENV["AWS__ACCESS_KEY"]
 }
 
 
