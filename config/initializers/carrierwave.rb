01624bc19e5945eac246d18c724789fa4870a5bf
加藤蒔朗 <rooji@katounoMacBook-Air.local>
2019-06-28 16:15:18 +0900

    credentials

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -8,8 +8,8 @@ CarrierWave.configure do |config|
     config.fog_provider = 'fog/aws'
     config.fog_credentials = {
       provider: 'AWS',
-      aws_access_key_id: ENV["AWS_ACCESS_KEY_ID"],
-      aws_secret_acccess_key: ENV["AWS_SECRET_ACCESS_KEY"],
+      aws_access_key_id: Rails.application.credentials.aws_access_key_id,
+      aws_secret_acccess_key: Rails.application.credentials.secret_key_base,
       region: 'ap-northeast-1'
     }
   else
