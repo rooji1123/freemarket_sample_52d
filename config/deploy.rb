d80941467ba7b655cd8d2dc09ac2f2f92fd625ab
加藤蒔朗 <rooji@katounoMacBook-Air.local>
2019-06-28 16:40:02 +0900

    s3

--- config/deploy.rb
+++ config/deploy.rb
@@ -7,6 +7,7 @@ set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', '
 set :rbenv_type, :user
 set :rbenv_ruby, '2.5.1'
 
+
 set :ssh_options, auth_methods: ['publickey'],
                   keys: ['~/.ssh/tech1899.pem']
 set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
@@ -23,9 +24,12 @@ end
 set :default_env, {
   rbenv_root: "/usr/local/rbenv",
   path: "/usr/local/rbenv/shims:/usr/local/rbenv/bin:$PATH",
-  AWS_ACCESS_KEY_ID: Rails.application.credentials.aws_access_key_id,
-  AWS_SECRET_ACCESS_KEY: Rails.application.credentials.secret_key_base
+  AWS_ACCESS_KEY_ID: ENV["AWS_ACCESS_KEY_ID"],
+  AWS_SECRET_ACCESS_KEY: ENV["AWS_SECRET_ACCESS_KEY"]
 }
+
+append :linked_files, 'config/database.yml', 'config/master.key'
+
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
