c2a920205bc987bcde6c7880c0e5809d1a5df0d1
加藤蒔朗 <rooji@katounoMacBook-Air.local>
2019-06-29 13:55:09 +0900

    Update carrierwave.rb

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -4,15 +4,14 @@ require 'carrierwave/storage/fog'
 
 CarrierWave.configure do |config|
   if Rails.env.production?
+    config.storage = :fog
     config.fog_provider = 'fog/aws'
     config.fog_credentials = {
-      aws_access_key_id: ENV["AWS_ACCESS_KEY_ID"],
-      aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
       provider: 'AWS',
+      aws_access_key_id: ENV["AWS_ACCESS_KEY_ID"],
+      aws_secret_acccess_key: ENV["AWS_SECRET_ACCESS_KEY"],
       region: 'ap-northeast-1'
     }
-    config.fog_directory  = 'freemarket52d'
-    config.cache_storage = :fog
   else
     config.storage :file
     config.enable_processing = false if Rails.env.test?
