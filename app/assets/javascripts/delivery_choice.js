f7540172e35c10df319dff4bb6d894cdc4e1f420
masatechexpert <cate35cate@gmail.com>
2019-07-01 12:43:39 +0900

    fix edit
    商品編集ページの修正

--- app/assets/javascripts/delivery_choice.js
+++ app/assets/javascripts/delivery_choice.js
@@ -11,15 +11,15 @@ $(function () {
       });
       $("#item_delivery_choice_id").append(
         ' <option value="---">---</option>\
-          <option value="0">未定</option>\
-          <option value="1">らくらくメルカリ便</option>\
-          <option value="2">ゆうメール</option>\
-          <option value="3">レターパック</option>\
-          <option value="4">普通郵便(定形、定形外)</option>\
-          <option value="5">クロネコヤマト</option>\
-          <option value="6">ゆうパック</option>\
-          <option value="7">クリックポスト</option>\
-          <option value="8">ゆうパケット</option>'
+          <option value="1">未定</option>\
+          <option value="2">らくらくメルカリ便</option>\
+          <option value="3">ゆうメール</option>\
+          <option value="4">レターパック</option>\
+          <option value="5">普通郵便(定形、定形外)</option>\
+          <option value="6">クロネコヤマト</option>\
+          <option value="7">ゆうパック</option>\
+          <option value="8">クリックポスト</option>\
+          <option value="9">ゆうパケット</option>'
       );
     } else if (fee_id == 2) {
       $('.sell-form__delivery__form-box__choice').css({
@@ -30,10 +30,10 @@ $(function () {
       });
       $("#item_delivery_choice_id").append(
         ' <option value="---">---</option>\
-          <option value="0">未定</option>\
-          <option value="1">クロネコヤマト</option>\
-          <option value="2">ゆうパック</option>\
-          <option value="3">ゆうメール</option>'
+          <option value="1">未定</option>\
+          <option value="2">クロネコヤマト</option>\
+          <option value="3">ゆうパック</option>\
+          <option value="4">ゆうメール</option>'
       );
     } else {
       $('.sell-form__delivery__form-box__choice').css({
--- app/assets/javascripts/edit_item.js
+++ app/assets/javascripts/edit_item.js
@@ -5,40 +5,27 @@ $(function () {
   var input_area = $(".input-area");
   var preview = $("#preview");
   var preview2 = $("#preview2");
-
-  // 登録済画像と新規追加画像を全て格納する配列（ビュー用）
   var images = [];
-  // 登録済画像データだけの配列（DB用）
   var registered_images_ids = []
-  // 新規追加画像データだけの配列（DB用）
   var new_image_files = [];
 
-
-  // 登録済画像のプレビュー表示
   gon.item_images.forEach(function (image, index) {
     var img = $(`<div class= "add_img"><div class="img_area"><img class="image"></div></div>`);
-
-    // カスタムデータ属性を付与
     img.data("image", index)
-
     var btn_wrapper = $('<div class="btn_wrapper"><a class="btn_edit">編集</a><a class="btn_delete">削除</a></div>');
 
-    // 画像に編集・削除ボタンをつける
     img.append(btn_wrapper);
 
     binary_data = gon.item_images_binary_datas[index]
 
-    // 表示するビューにバイナリーデータを付与
     img.find("img").attr({
       src: "data:image/jpeg;base64," + binary_data
     });
 
-    // 登録済画像のビューをimagesに格納
     images.push(img)
     registered_images_ids.push(image.id)
   })
 
-  // 画像が４枚以下のとき
   if (images.length <= 4) {
     $('#preview').empty();
     $.each(images, function (index, image) {
@@ -49,7 +36,6 @@ $(function () {
       'width': `calc(100% - (20% * ${images.length}))`
     })
 
-    // 画像が５枚のとき１段目の枠を消し、２段目の枠を出す
   } else if (images.length == 5) {
     $("#preview").empty();
     $.each(images, function (index, image) {
@@ -64,22 +50,17 @@ $(function () {
     });
     preview2.empty();
 
-    // 画像が６枚以上のとき
   } else if (images.length >= 6) {
-    // １〜５枚目の画像を抽出
     var pickup_images1 = images.slice(0, 5);
 
-    // １〜５枚目を１段目に表示
     $('#preview').empty();
     $.each(pickup_images1, function (index, image) {
       image.data('image', index);
       preview.append(image);
     })
 
-    // ６枚目以降の画像を抽出
     var pickup_images2 = images.slice(5);
 
-    // ６枚目以降を２段目に表示
     $.each(pickup_images2, function (index, image) {
       image.data('image', index + 5);
       preview2.append(image);
@@ -96,7 +77,6 @@ $(function () {
       'width': `calc(100% - (20% * ${images.length - 5}))`
     })
 
-    // 画像が１０枚になったら枠を消す
     if (images.length == 10) {
       dropzone2.css({
         display: "none"
@@ -110,7 +90,6 @@ $(function () {
   input_area.append(new_image);
 
 
-  // 画像を新しく追加する場合
   $("#edit_item .item__img__dropzone, #edit_item .item__img__dropzone2").on("change", 'input[type= "file"].upload-image', function () {
     var file = $(this).prop("files")[0];
     new_image_files.push(file)
@@ -120,7 +99,6 @@ $(function () {
     reader.onload = function (e) {
       var btn_wrapper = $('<div class="btn_wrapper"><a class="btn_edit">編集</a><a class="btn_delete">削除</a></div>');
 
-      // 画像に編集・削除ボタンをつける
       img.append(btn_wrapper);
       img.find("img").attr({
         src: e.target.result
@@ -130,7 +108,6 @@ $(function () {
     reader.readAsDataURL(file);
     images.push(img);
 
-    // 画像が４枚以下のとき
     if (images.length <= 4) {
       $('#preview').empty();
       $.each(images, function (index, image) {
@@ -141,7 +118,6 @@ $(function () {
         'width': `calc(100% - (20% * ${images.length}))`
       })
 
-      // 画像が５枚のとき１段目の枠を消し、２段目の枠を出す
     } else if (images.length == 5) {
       $("#preview").empty();
       $.each(images, function (index, image) {
@@ -156,10 +132,8 @@ $(function () {
       });
       preview2.empty();
 
-      // 画像が６枚以上のとき
     } else if (images.length >= 6) {
 
-      // 配列から６枚目以降の画像を抽出
       var pickup_images = images.slice(5);
 
       $.each(pickup_images, function (index, image) {
@@ -170,7 +144,6 @@ $(function () {
         });
       });
 
-      // 画像が１０枚になったら枠を消す
       if (images.length == 10) {
         dropzone2.css({
           display: "none"
@@ -185,22 +158,15 @@ $(function () {
   });
 
 
-  // 削除ボタン
   $("#edit_item .item__img__dropzone, #edit_item .item__img__dropzone2").on('click', '.btn_delete', function () {
 
-    // 削除ボタンを押した画像を取得
     var target_image = $(this).parent().parent();
-
-    // 削除画像のdata-image番号を取得
     var target_image_num = target_image.data('image');
 
-    // 対象の画像をビュー上で削除
     target_image.remove();
 
-    // 対象の画像を削除した新たな配列を生成
     images.splice(target_image_num, 1);
 
-    // target_image_numが登録済画像の数以下の場合は登録済画像データの配列から削除、それより大きい場合は新たに追加した画像データの配列から削除
     if (target_image_num < registered_images_ids.length) {
       registered_images_ids.splice(target_image_num, 1);
     } else {
@@ -213,8 +179,6 @@ $(function () {
       })
     }
 
-    // 削除後の配列の中身の数で条件分岐
-    // 画像が４枚以下のとき
     if (images.length <= 4) {
       $('#preview').empty();
       $.each(images, function (index, image) {
@@ -229,7 +193,6 @@ $(function () {
         'display': 'none'
       })
 
-      // 画像が５枚のとき１段目の枠を消し、２段目の枠を出す
     } else if (images.length == 5) {
       $('#preview').empty();
       $.each(images, function (index, image) {
@@ -246,23 +209,14 @@ $(function () {
         'display': 'none'
       })
       preview2.empty();
-
-      // 画像が６枚以上のとき
     } else {
-      // １〜５枚目の画像を抽出
       var pickup_images1 = images.slice(0, 5);
-
-      // １〜５枚目を１段目に表示
       $('#preview').empty();
       $.each(pickup_images1, function (index, image) {
         image.data('image', index);
         preview.append(image);
       })
-
-      // ６枚目以降の画像を抽出
       var pickup_images2 = images.slice(5);
-
-      // ６枚目以降を２段目に表示
       $.each(pickup_images2, function (index, image) {
         image.data('image', index + 5);
         preview2.append(image);
@@ -276,25 +230,18 @@ $(function () {
 
 
   $('.edit_item').on('submit', function (e) {
-    // 通常のsubmitイベントを止める
     e.preventDefault();
-    // images以外のform情報をformDataに追加
     var formData = new FormData($(this).get(0));
-
-    // 登録済画像が残っていない場合は便宜的に0を入れる
     if (registered_images_ids.length == 0) {
       formData.append("registered_images_ids[ids][]", 0)
-      // 登録済画像で、まだ残っている画像があればidをformDataに追加していく
     } else {
       registered_images_ids.forEach(function (registered_image) {
         formData.append("registered_images_ids[ids][]", registered_image)
       });
     }
 
-    // 新しく追加したimagesがない場合は便宜的に空の文字列を入れる
     if (new_image_files.length == 0) {
       formData.append("new_images[images][]", " ")
-      // 新しく追加したimagesがある場合はformDataに追加する
     } else {
       new_image_files.forEach(function (file) {
         formData.append("new_images[images][]", file)
