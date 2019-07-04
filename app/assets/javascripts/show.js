97613b210e4ba49060c41ec0efe12ba7be18eb17
masayoshiishitsuka <52.ishitsuka.masayoshi>
2019-07-04 11:00:59 +0900

    fix autoplay

--- app/assets/javascripts/show.js
+++ app/assets/javascripts/show.js
@@ -6,25 +6,15 @@ $(function(){
    var index = $(thumbnailItem).index(this);
    $(this).attr("data-index",index);
   });
-  
-  $(slider).on('init',function(slick){
-   var index = $(".slide-item.slick-slide.slick-current").attr("data-slick-index");
-   $(thumbnailItem+'[data-index="'+index+'"]').addClass("thumbnail-current");
-  });
 
   $(slider).slick({
     arrows: false,
-    infinite: false
+    infinite: false,
+    waitForAnimate: false
   });
   $(thumbnailItem).on('mouseover',function(){
     var index = $(this).attr("data-index");
     $(slider).slick("slickGoTo",index,false);
   });
   
-  $(slider).on('beforeChange',function(event,slick, currentSlide,nextSlide){
-    $(thumbnailItem).each(function(){
-      $(this).removeClass("thumbnail-current");
-    });
-    $(thumbnailItem+'[data-index="'+nextSlide+'"]').addClass("thumbnail-current");
-  });
 });
